b716675ed38e89b80e723ae2abe8bec6f2a1968c
SivaKumarKonduru <siva.eswark7@gmail.com>
2025-06-16 15:15:27 +0530

    added module structure

---
+++ AWS-Infra/AWS-Env/main.tf
@@ -0,0 +1,10 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "modules" {
+  source = "./modules"
+  ami_value = "ami-0616df5cb3508aa23" # replace this
+  instance_type_value = "t2.micro"
+  subnet_id_value = "subnet-0fc4e71d5d8e5205e". # replace this
+}
